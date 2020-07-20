8c398d4a5ad7917fc3ef8aa1224601fa66b67172
amazingrecordingstudios <amazingrecordingstudios@gmail.com>
2020-07-20 10:19:30 +0200

    Adding basic Espresso tests, following https://developer.android.com/training/testing/espresso/basics

---
+++ app/src/androidTest/java/it/amazingrecordingstudios/hippo/HelloWorldEspressoTest.java
@@ -0,0 +1,38 @@
+package it.amazingrecordingstudios.hippo;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HelloWorldEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    //TODO FIXME after each test return to main window
+
+    @Test
+    public void exampleBasicEspressoTest() {
+        onView(withId(R.id.greekShortTextTV))
+                .perform(click())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
